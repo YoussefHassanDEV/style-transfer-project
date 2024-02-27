9690f91b3bac1cfeadb608306fccbbd74542de51
YoussefHassan <devyoussefhassan@gmail.com>
2024-02-27 20:39:56 +0200

    navbar pre
    navbar pre

--- StyleTransferFrontend/src/app/app.module.ts
+++ StyleTransferFrontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { InputComponent } from './components/input/input.component';
+import { OutputComponent } from './components/output/output.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InputComponent,
+    OutputComponent
   ],
   imports: [
     BrowserModule,
